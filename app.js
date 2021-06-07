2cb10363bf8dfdd4ec7b4f9edb1b74e4435b0e20
문한샘 <jbookes@naver.com>
2021-06-07 10:26:30 +0900

    [문한샘] - 초기

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const path = require("path");
+// const nunjucks = require("nunjucks");
+
+dotenv.config();
+const app = express();
+app.set("port", process.env.PORT || 19082);
+app.set("view engine", "html");
+// nunjucks.configure("views", {
+//   express: app,
+//   watch: true,
+// });
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(app.get("port"), () => {
+  console.log(`http://localhost:${app.get("port")}`);
+});
