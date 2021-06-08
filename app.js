b92b4360ac4097880b61b5e45bcee2bf12327e13
gyeongseo944 <gyeongseo944@gmail.com>
2021-06-08 16:38:12 +0900

    [박경서] app.js model router 삭제

--- app.js
+++ app.js
@@ -2,43 +2,39 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const path = require("path");
-const sequelize = require('sequelize');
-const models = require('./models/index');
+const sequelize = require("sequelize");
+const models = require("./models/index");
 const nunjucks = require("nunjucks");
 
-//routers 
-const index_router = require('./routes/index');
-const model_manage_board_router = require('./routes/model_manage_board');
-
+//routers
+const index_router = require("./routes/index");
 
 models.sequelize
-	.sync()
-	.then(() => {
-		console.log("DB connected");
-	})
-	.catch((err) => {
-		console.log(`DB connection fail: ${err}`);
-	});
+  .sync()
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(`DB connection fail: ${err}`);
+  });
 
 dotenv.config();
 const app = express();
 app.set("port", process.env.PORT || 4000);
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "html");
 nunjucks.configure("views", {
-	express: app,
-	watch: true,
+  express: app,
+  watch: true,
 });
 
-
-app.get('/model_manage_board', model_manage_board_router)
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/', index_router);
+app.use("/", index_router);
 
 app.listen(app.get("port"), () => {
-	console.log(`http://localhost:${app.get("port")}`);
+  console.log(`http://localhost:${app.get("port")}`);
 });
