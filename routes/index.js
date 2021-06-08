21d442a84788b8323be13869f619ec63e0c4d5ce
ykim5470 <ykim5470@uni.sydeny.edu.au>
2021-06-08 18:20:46 +0900

    [김윤호] - model_input & output table data insert

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,10 @@ router.get("/", function (req, res, next) {
   res.render("index");
 })
 router.get("/analysis/plus",analysis.output.plus)
-router.get("/analysis/view",analysis.output.show)
 router.get("/model_manage_board", auth.output.model_manage_board)
 router.get("/model_register_board", auth.output.model_register_board)
 
 //router.post
-router.post("/analysis/insert",analysis.process.insert);
+router.post("/analysis/insert", analysis.process.insert);
+router.post("/model_register_board", auth.process.model_register_board)
 module.exports = router;
