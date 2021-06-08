d78d5756a90b4ea22ed8fae2b637a22c34e67a4a
ykim5470 <ykim5470@uni.sydeny.edu.au>
2021-06-08 15:23:56 +0900

    [김윤호] - app.js & routes/model_manage_board.js indent change

--- app.js
+++ app.js
@@ -2,29 +2,30 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const path = require("path");
-const sequelize = require('sequelize')
-const models = require('./models/index')
+const sequelize = require("sequelize");
+const models = require("./models/index");
 const nunjucks = require("nunjucks");
-const model_manage_board_router = require('./routes/model_manage_board')
 
 
-models.sequelize.sync().then(() => {
-  console.log('DB connected')
-}).catch(err => {
-  console.log(`DB connection fail: ${err}`)
-})
+models.sequelize
+	.sync()
+	.then(() => {
+		console.log("DB connected");
+	})
+	.catch((err) => {
+		console.log(`DB connection fail: ${err}`);
+	});
 
 dotenv.config();
 const app = express();
 app.set("port", process.env.PORT || 4000);
 app.set("view engine", "html");
 nunjucks.configure("views", {
-  express: app,
-  watch: true,
+	express: app,
+	watch: true,
 });
 
-
-app.get('/model_manage_board', model_manage_board_router)
+app.get("/model_manage_board", model_manage_board_router);
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
@@ -32,5 +33,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(app.get("port"), () => {
-  console.log(`http://localhost:${app.get("port")}`);
+	console.log(`http://localhost:${app.get("port")}`);
 });
