c70c0b9cb1a90a344b4ab4ddf6c7ec5f4d02bb1d
ykim5470 <ykim5470@uni.sydeny.edu.au>
2021-06-08 14:41:13 +0900

    [김윤호 index.html title change, nunjucks page render

--- routes/model_manage_board.js
+++ routes/model_manage_board.js
@@ -2,16 +2,25 @@ const express = require('express')
 const router = express.Router()
 const { model_list, model_output, model_input } = require('../models')
 
-// 모델 관리 대시보드
-// router.get('/model_manage_board', (req, res) => {
-//     console.log('This is model management board')
-    
-// })
-
-
 // 모델 관리 대시보드 페이지 
-router.get('/model_manage_board', (req, res) => {
-   return res.render('model/model_manage_board', {data: 'a'})
+router.get('/model_manage_board', async (req, res) => {
+    try {
+        const data = await model_list.findAll()
+        return res.render('model/model_manage_board', { test: 'a', allList: data })
+    }catch(err){
+        return res.status(500).json({error: 'Something went wrong'})
+    }
+})
+
+
+router.post('/model_manage_board', async (req, res) => {
+    try {
+        const { md_id, al_time, md_name, al_name_mo, run_status } = req.body
+        const data = await model_list.create({ md_id, al_time, md_name, al_name_mo, run_status})
+        return res.json(data)
+    }catch(err){
+        return res.status(500).json({error: 'Something went wrong'})
+    }
 })
 
 module.exports = router
