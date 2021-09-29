85e9f2fa6f219210c4a6b19808278861b67b02e8
ykim5470 <hubert8663@gmail.com>
2021-09-29 13:25:12 +1000

    [김윤호] - JSON tree view 테스트 파일 생성 - cookie 세팅 변수 변환

--- api/tokenToJson.js
+++ api/tokenToJson.js
@@ -1,15 +1,16 @@
-const base64 = require("base-64");
+const base64 = require('base-64')
 
-exports.tokenToJson = async function (req, res) {
-    try {
-      const token = await req.cookies.token;
-  
-      let tokenBase64 = token.split(".")[1];
-      let tokenStr = base64.decode(tokenBase64); // 복호화
-      let tokenToJson = JSON.parse(tokenStr);
-      return tokenToJson
-    } catch (error) {
-      console.log(error);
-      res.redirect("/");
-    }
-};
+exports.tokenToJson = async function (req, res, access_token) {
+  try {
+    // const token = await req.cookies.token;
+    const token = access_token
+
+    let tokenBase64 = token.split('.')[1]
+    let tokenStr = base64.decode(tokenBase64) // 복호화
+    let tokenToJson = JSON.parse(tokenStr)
+    return tokenToJson
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+}
